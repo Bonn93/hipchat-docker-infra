e90913de476555b214636ff2a213a0edd44c2d8f
bonn93 <maxbernnelson@gmail.com>
2018-01-03 11:52:14 +1100

    Added pgpool1 and pgpool2. Added Mixed Trust and MD5 for Replication/PGPool. Fixed Random Shit.

--- pgsql/pgpool/bin/entrypoint.sh
+++ pgsql/pgpool/bin/entrypoint.sh
@@ -9,7 +9,7 @@ export POOL_PASSWD_FILE='/usr/local/etc/pool_passwd'
 echo "host all all 0.0.0.0/0 trust" >> /etc/pgpool2/pool_hba.conf
 #echo "host hipchat hipchat 0.0.0.0/0 md5" /etc/pgpool2/pool_hba.conf
 #echo "hipchat" | pg_md5 --username hipchat -m hipchat -p
-#echo "hipchat hipchat" >> /etc/pgpool2/pool_passwd
+#echo "hipchat hipchat" > /etc/pgpool2/pool_passwd
 
 echo '>>> STARTING SSH (if required)...'
 source /home/postgres/.ssh/entrypoint.sh
