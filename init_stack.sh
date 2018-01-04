d273180d9425ffe9a5f30d4be94f1c1dc65923ab
bonn93 <maxbernnelson@gmail.com>
2018-01-04 16:03:04 +1100

    Updated init output

--- init_stack.sh
+++ init_stack.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
-PORTS=(2049 5432 6379 1936 666)
-
+PORTS=(2049 5432 6379 1936 666 9000 5430)
 
 # Ensure dirs are present prior to compose
 mkdir -p $HOME/dockerdata/nfs_hipchat $HOME/dockerdata/rds_hipchat/ $HOME/dockerdata/psql_hipchat/ $HOME/dockerdata/ha_hipchat/ $HOME/dockerdata/pgpool/ $HOME/dockerdata/pgmaster $HOME/dockerdata/pgslave1 $HOME/dockerdata/pgslave2
 # Copy NFS Export CFG to Dockerdata
 cp -f nfs-export-cfg $HOME/dockerdata/nfs_hipchat/nfs-export-cfg
-#cp -f pgpool/pgpool.conf $HOME/dockerdata/pgpool/
-#cp -f pgsql/pgsql/configs/pg_hba.conf $HOME/dockerdata/psql_hipchat/
 # Test ports on localhost are not in use
 #printf "Testing the following ports for availability:\n"
 #printf "%s\n" "${PORTS[@]}"
 #{
 #for element in "${PORTS[@]}";
 #do
-#    nc -zv -w 1 127.0.0.1 ${PORTS[0]}; nc -zv -w 1 127.0.0.1 ${PORTS[1]}; nc -zv -w 1 127.0.0.1 ${PORTS[2]}; nc -zv -w 1 127.0.0.1 ${PORTS[3]}; nc -zv -w 1 127.0.0.1 ${PORTS[4]};
+#    nc -zv -w 1 127.0.0.1 ${PORTS[0]}; nc -zv -w 1 127.0.0.1 ${PORTS[1]}; nc -zv -w 1 127.0.0.1 ${PORTS[2]}; nc -zv -w 1 127.0.0.1 ${PORTS[3]}; nc -zv -w 1 127.0.0.1 ${PORTS[4]}; nc -zv -w 1 127.0.0.1 ${PORTS[5]}; nc -zv -w 1 127.0.0.1 ${PORTS[6]}; nc -zv -w 1 127.0.0.1 ${PORTS[7]};
 #done
 #} &> /dev/null
 #    if [ $? -eq 1 ]
 #        then
 #            echo "Port Test Passed"
 #    else
-#        echo "Port test failed, check ports 2049-NFS, 5432-POSTGRES, 666-HAPROXY, 1936-HAPROXY and 6379-REDIS"
+#        echo "Port test failed, check ports listed in docker-compose*.yml"
 #        echo  "This failure means a port is already in use, and the docker compose will fail"
 #        exit 1
 #    fi
@@ -49,6 +46,8 @@ printf "docker exec -it container_name sh\n"
 printf "Note some alpine containers do not have bash!\n"
 printf "Here's a list of the running container names:\n"
 docker ps | awk '{print $NF}' | grep -e '/*_hipchat'
-printf "Load Balancer Status Page: http://localohost:1936\n"
-printf "Your connection for Redis, Postgres and NFS should be the primary IP address of your workstation\n"
+printf "Load Balancer Status Page: http://localhost:1936\n"
+printf "Redis = <workstation_ip> + Port 9000\n"
+printf "Postgres = <workstation_ip> + Port 5430"
+printf "NFS = <workstation_ip> + /\n"
 exit 0
