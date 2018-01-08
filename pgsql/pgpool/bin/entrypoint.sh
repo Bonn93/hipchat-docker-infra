ee3b2b3738c89c5f79111bee5d73d1d021d14f42
bonn93 <maxbernnelson@gmail.com>
2018-01-08 12:53:42 +1100

    Fixed missing configs in pgpool

--- pgsql/pgpool/bin/entrypoint.sh
+++ pgsql/pgpool/bin/entrypoint.sh
@@ -6,17 +6,10 @@ export PCP_FILE='/usr/local/etc/pcp.conf'
 export HBA_FILE='/usr/local/etc/pool_hba.conf'
 export POOL_PASSWD_FILE='/usr/local/etc/pool_passwd'
 
-#echo "host all all 0.0.0.0/0 trust" >> /etc/pgpool2/pool_hba.conf
-#touch /usr/local/etc/passwd
-#echo "hipchat:hipchat" >> /usr/local/etc/passwd
-pg_md5 -u hipchat -m hipchat -f pgpool.conf
+touch /usr/local/etc/passwd
 
-
-#echo "host all all 0.0.0.0/0 md5" /etc/pgpool2/pool_hba.conf
-#echo "host hipchat hipchat 0.0.0.0/0 md5" /etc/pgpool2/pool_hba.conf
-#echo "hipchat" | pg_md5 --username hipchat -m hipchat -p
-#echo "hipchat:08b4beb3dbcb40739e1b3feff15fbe98" > /etc/pgpool2/pool_passwd
-#echo "hipchat:hipchat" > /usr/local/etc/pool_passwd
+cp -a /etc/pgpool2/. /usr/local/etc/
+pg_md5 -u hipchat -m hipchat -f /usr/local/etc/pgpool.conf
 
 
 echo '>>> STARTING SSH (if required)...'
